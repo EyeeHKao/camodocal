d1efdbe61ef626fd0f986969cdcfd909cfda0e03
EyeeH <990457372@qq.com>
2019-11-28 11:33:37 +0800

    uncomment odometer-camera extrinsics calibrate

--- include/camodocal/calib/CamOdoCalibration.h
+++ include/camodocal/calib/CamOdoCalibration.h
@@ -6,12 +6,17 @@
 
 namespace camodocal
 {
-
+/*@brief
+    odometer-camera 外参校正类，主要参考了这篇文献An analytical least-squares solution to the odometer-camera extrinsic calibration problem
+CX Guo ，FM Mirzaei ，SI Roumeliotis  - 10.1109/ICRA.2012.6225339
+*/
+   
 class CamOdoCalibration
 {
 public:
     CamOdoCalibration();
-
+    
+    //添加相对应的里程计估计的相对位姿和相机估计的相对位姿态数据，这里的数据最好要做时间同步处理
     bool addMotionSegment(const std::vector<Eigen::Matrix4d, Eigen::aligned_allocator<Eigen::Matrix4d> >& H_cam,
                           const std::vector<Eigen::Matrix4d, Eigen::aligned_allocator<Eigen::Matrix4d> >& H_odo);
 
@@ -22,14 +27,17 @@ public:
     size_t getMotionCount(void) const;
     void setMotionCount(size_t count);
 
+    //外参校正/标定
     bool calibrate(Eigen::Matrix4d& H_cam_odo);
 
+    //读盘或写盘
     bool readMotionSegmentsFromFile(const std::string& filename);
     bool writeMotionSegmentsToFile(const std::string& filename) const;
 
     bool getVerbose(void);
     void setVerbose(bool on = false);
-
+    
+    //估计外参和尺度，
     bool estimate(const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& rvecs1,
                   const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& tvecs1,
                   const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& rvecs2,
@@ -38,19 +46,20 @@ public:
                   std::vector<double>& scales) const;
 
 private:
+    //估计q_yz
     bool estimateRyx(const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& rvecs1,
                      const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& tvecs1,
                      const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& rvecs2,
                      const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& tvecs2,
                      Eigen::Matrix3d& R_yx) const;
-    
+    //校正估计值
     void refineEstimate(Eigen::Matrix4d& H_cam_odo, std::vector<double>& scales,
                         const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& rvecs1,
                         const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& tvecs1,
                         const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& rvecs2,
                         const std::vector<std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >& tvecs2) const;
 
-    // solve ax^2 + bx + c = 0
+    // 求解一元二次方程：solve ax^2 + bx + c = 0
     bool solveQuadraticEquation(double a, double b, double c, double& x1, double& x2) const;
 
     typedef struct
@@ -59,19 +68,19 @@ private:
 
         Eigen::Vector3d rotation;
         Eigen::Vector3d translation;
-    } Motion;
+    } Motion;   ///一次运动(相邻两时刻的相对位姿)数据结构
 
     typedef struct
     {
         EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-        std::vector<Motion, Eigen::aligned_allocator<Motion> > odoMotions;
-        std::vector<Motion, Eigen::aligned_allocator<Motion> > camMotions;
-    } MotionSegment;
+        std::vector<Motion, Eigen::aligned_allocator<Motion> > odoMotions;  ///多次里程计运动
+        std::vector<Motion, Eigen::aligned_allocator<Motion> > camMotions;  ///多次相机运动
+    } MotionSegment;    ///多次运动数据结构
 
     std::vector<MotionSegment> mSegments;
 
-    size_t mMinMotions;
+    size_t mMinMotions; ///标定所需最小运动次数
 
     bool mVerbose;
 };
